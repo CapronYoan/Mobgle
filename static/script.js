8919fd995defb10b153e679e1669ad5e6fea8d3b
CapronYoan <capronyoan@gmail.com>
2020-11-05 21:12:52 +0100

    second commit

---
+++ static/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelector(".form-control").value = "";
+
+    document.querySelector(".btn").disabled = true;
+
+    document.querySelector(".form-control").onkeyup = () => {
+    if (document.querySelector(".form-control").value.length > 0)
+        document.querySelector(".btn").disabled = false;
+    else
+        document.querySelector(".btn").disabled = true;
+    }
+})
